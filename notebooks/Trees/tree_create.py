26d604fcd9d3575963898c83bf685a70bde9a444
Carter McClellan <carter.s.mcclellan@gmail.com>
2020-02-14 13:06:33 -0800

    Update Trees Module

---
+++ notebooks/Trees/tree_create.py
@@ -0,0 +1,18 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def create_bin_tree_helper(vals):
+    if not vals: return None
+    mid = len(vals)//2
+    node = TreeNode(vals[mid])
+    node.left = create_bin_tree_helper(vals[:mid])
+    node.right = create_bin_tree_helper(vals[mid+1:])
+    return node
+
+def create_bin_tree(vals):
+    vals = sorted(vals) # sort values before input
+    root = create_bin_tree_helper(vals)
+    return root
